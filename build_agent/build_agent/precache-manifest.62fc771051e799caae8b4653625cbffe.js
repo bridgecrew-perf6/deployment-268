136b117ae27406270e06cfbf76d2135c0dd9f665
arawna <alihocaoglu1220@gmail.com>
2022-01-31 14:05:37 +0300

    first commit

---
+++ build_agent/build_agent/precache-manifest.62fc771051e799caae8b4653625cbffe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29937b66ec509ffc01505ee02609beba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30329d6264b3591c8f02",
+    "url": "/static/css/2.15e04fad.chunk.css"
+  },
+  {
+    "revision": "7fcdd0b08b6fbbeab910",
+    "url": "/static/css/main.9e613c20.chunk.css"
+  },
+  {
+    "revision": "30329d6264b3591c8f02",
+    "url": "/static/js/2.f4a67bde.chunk.js"
+  },
+  {
+    "revision": "07b458057b78da485a37e04e3e80378c",
+    "url": "/static/js/2.f4a67bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fcdd0b08b6fbbeab910",
+    "url": "/static/js/main.2d8eb8e6.chunk.js"
+  },
+  {
+    "revision": "dd2b242d605e30b8660e",
+    "url": "/static/js/runtime-main.e747401b.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
